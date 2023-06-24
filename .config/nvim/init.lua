ddc7b5f4ae2f1703ac4c3f1a97402dd745761b85
Andrew Patrick Miller <andrew.patrick.miller.2021@gmail.com>
2023-06-24 15:49:55 -0600

    📅  | Sat Jun 24 15:49:55 MDT 2023

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,11 +1,12 @@
 print 'sourced ~/.config/nvim/init.lua'
+vim.cmd('source ~/.config/nvim/vim-plug.vim')
 -- require the file name maps to lua directory which is on the predefined path.
 require('maps')
 require('copilot-maps')
 require('variables')
+require('lua-lsp')
 
 
-vim.cmd('source ~/.config/nvim/vim-plug.vim')
 
 vim.cmd([[
   syntax off

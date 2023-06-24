108338b59d328aa6ad0180827dc5988cb0385fc6
Andrew Patrick Miller <andrew.patrick.miller.2021@gmail.com>
2023-06-23 19:05:29 -0600

    added vim "set" variables

---
+++ .config/nvim/init.lua
@@ -0,0 +1,6 @@
+print 'sourced ~/.config/nvim/init.lua'
+-- require the file name maps to lua directory which is on the predefined path.
+require('maps')
+require('variables')
+
+
---
+++ .config/nvim/lua/variables.lua
@@ -0,0 +1,32 @@
+print "Loading variables.lua"
+vim.opt.modifiable = true
+vim.opt.lazyredraw = true
+vim.opt.incsearch = true
+vim.opt.autochdir = true
+vim.opt.autoindent = true
+vim.opt.cursorline = true
+vim.opt.expandtab = true
+vim.opt.laststatus = 2
+vim.opt.swapfile = false
+vim.opt.number = true
+vim.opt.shiftwidth = 2
+vim.opt.showcmd = true
+vim.opt.statusline = '%F'
+vim.opt.tabstop = 2
+vim.opt.updatetime = 100
+vim.opt.encoding = 'utf-8'
+vim.opt.hidden = true
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.cmdheight = 2
+vim.opt.updatetime = 300
+vim.opt.shortmess = vim.opt.shortmess + 'c'
+
+vim.g.netrw_bufsettings = 'noma nomod nu nobl nowrap ro'
+
+vim.cmd([[
+	syntax off
+	filetype on
+]])
+
+

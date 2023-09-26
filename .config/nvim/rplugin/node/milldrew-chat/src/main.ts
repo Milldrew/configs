583dc761593263bf87af83afa38d2c3f8f7a5d5c
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2023-09-25 18:45:55 -0600

    📅  | Mon Sep 25 18:45:54 MDT 2023

--- .config/nvim/rplugin/node/milldrew-chat/src/main.ts
+++ .config/nvim/rplugin/node/milldrew-chat/src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as neovim from 'neovim';
+import { NvimPlugin, Plugin, attach } from 'neovim';
 
-module.exports = async function bootstrap(plugin) {
-  // const app = await NestFactory.create(AppModule);
+async function bootstrap(plugin: NvimPlugin) {
+  console.log('hi');
+  const nvim = plugin.nvim;
+  // const servername = await nvim.getVvar('servername').catch(console.log);
+  // console.log('servername', servername);
   function setLine() {
+    // plugin.nvim.setLine('A line, for your troubles' + servername);
     plugin.nvim.setLine('A line, for your troubles');
   }
+
   plugin.registerCommand('SetLine', [plugin.nvim.buffer, setLine]);
-};
+}
+module.exports = bootstrap;
+console.log(neovim);
